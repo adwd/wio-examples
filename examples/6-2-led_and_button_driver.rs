ad5429e713424e49fd61c8fd88356470677899d3
adwd <adwd@users.noreply.github.com>
2022-06-16 19:08:49 +0900

    initial commit

---
+++ examples/6-2-led_and_button_driver.rs
@@ -0,0 +1,44 @@
+//! 6-2 LEDとボタン/GPIOのサンプルコードです。
+//! ボタン1 (一番右のボタン) を押している間、ユーザーLEDが点灯します。
+//! LEDドライバとボタンドライバを導入したバージョンです。
+//!
+//! ### 実行方法
+//! ```sh
+//! $ cargo hf2 --example 6-2-led_and_button_driver
+//! ```
+
+#![no_std]
+#![no_main]
+#![allow(dead_code)] // 使用しないメソッドでコンパイラが警告を出さないようにする
+
+use panic_halt as _;
+use wio_terminal as wio;
+
+use wio::entry;
+use wio::hal::gpio::*; // GPIOの構造体やトレイトをインポートする
+use wio::pac::Peripherals;
+use wio::prelude::*; // 主要な構造体やトレイトをインポートする
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take().unwrap();
+    let mut pins = wio::Pins::new(peripherals.PORT);
+
+    // TODO: ボタン1を押している間、LEDが点灯するコードを書く
+    let mut led = pins.user_led.into_push_pull_output(&mut pins.port);
+    let button1 = pins.button1.into_floating_input(&mut pins.port);
+
+    loop {
+        if button1.is_low().unwrap() {
+            led.set_high().unwrap();
+        } else {
+            led.set_low().unwrap();
+        }
+    }
+}
+
+// Wio Terminalのボタン1ドライバ
+// TODO: Button1 を実装する
+
+// Wio TerminalのユーザーLEDドライバ
+// TODO: Led を実装する
